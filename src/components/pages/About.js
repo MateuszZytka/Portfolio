3d8aa038681f23e24fea4ef109ee7917881f6e6d
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-11-19 20:16:41 -0500

    added file structure, navbar and logo

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+    return ( 
+        <div>
+        <h1>About Me</h1>
+        <p>
+        I am currently enrolled in the Full Stack Developer coding program at University of Toronto School of Continued Studies. I come from a background of working in the trades, specifacally HVAC. My previous field was one of constant learning and problem solving, two traits that I have found to be relatable to learning to code. Learning to work indepentantly as well as part of a team was critical to my former field as the scope of jobs varied heavily. I am excited to start my new journey into the ever changing field of technology. Below you'll find the projects I have worked on so far with more to come!
+        </p>
+      </div>
+    )
+}
