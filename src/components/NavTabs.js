13496614c75105612f8ef0e596743303e611cdab
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-11-24 21:18:27 -0500

    updated resume page and functionality of buttons

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -6,7 +6,7 @@ const ScrollLink = Scroll.ScrollLink
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function NavTabs({ currentPage, handlePageChange }) {
+function NavTabs({currentPage, handlePageChange}) {
   return (
     <div className='sidebar'>
       
@@ -14,46 +14,38 @@ function NavTabs({ currentPage, handlePageChange }) {
       {/* <li><h3 className='nav-header'>Navigation</h3></li> */}
         <li>
         
-          <button
-            href="#about"
-            onClick={() => handlePageChange('About')}
+          <a href = "#about"><button
             // This is a conditional (ternary) operator that checks to see if the current page is "Home"
             // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
             className={currentPage === 'About' ? 'button-active' : ''}
           >
             About Me
-          </button>
+          </button> </a>
           {/* </ScrollLink> */}
         </li>
         <li>
-          <button 
-            href="#projects"
-            onClick={() => handlePageChange('Projects')}
+          <a href = "#projects"><button 
             // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Projects' ? 'button-active' : ''}
           >
             Projects
-          </button>
+          </button></a>
         </li>
         <li>
-          <button
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
+          <a href = "#contact"><button
             // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Contact' ? 'button-active' : ''}
           >
             Contact
-          </button>
+          </button></a>
         </li>
         <li>
-          <button
-            href="#resume"
-            onClick={() => handlePageChange('Contact')}
+          <a href = "#resume"><button
             // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'resume' ? 'button-active' : ''}
           >
             Resume
-          </button>
+          </button></a>
         </li>
       </ul>
     </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,34 @@ import ResumeDownload from '../../images/Resume -Mateusz Zytka.pdf'
 export default function Resume() {
     return ( 
         <div id='resume' className='resume'>
-          <h2>Click to download my Resume</h2>
+          
+        <div className='technologies'>
+          <div className='front'>
+            <h2>Front End Technologies and Frameworks</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Bulma</li>
+              <li>Bootstrap</li>
+              <li>Jquery</li>
+              <li></li>
+            </ul>
+          </div>
+          <div className='back'>
+            <h2>Back End Technolgies</h2>
+            <ul>
+              <li>Express</li>
+              <li>SQL</li>
+              <li>NoSQL</li>
+              <li>MongoDB</li>
+              <li>Node.JS</li>
+              <li>RESTful APIs</li>
+            </ul>
+          </div>
+        </div>
+        <h2>Click to download my Resume</h2>
         <a href={ResumeDownload} download> <button>Download</button>  </a>
       </div>
     )
